bca2b5f389133c2950c03e9bf5bb700a9fcb4323
Matthew <ng.manhtruong1996@gmail.com>
2024-02-20 23:23:38 +0700

    fix output directory

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-static";
+// import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 // import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
