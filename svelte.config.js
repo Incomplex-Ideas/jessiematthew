7ec0b99ab3ad4220c65e0dbd5d5870c261ecf947
Matthew <ng.manhtruong1996@gmail.com>
2024-02-22 20:44:33 +0700

    set default adapter

--- svelte.config.js
+++ svelte.config.js
@@ -13,14 +13,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      fallback: "error.html",
-      strict: false,
-      routes: {
-        include: ["/*"],
-        exclude: ["<all>"],
-      },
-    }),
+    adapter: adapter(),
     // csp: {
     //   directives: {
     //     "script-src": ["self"],
