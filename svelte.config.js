54917394f604e182986dda8007b6c8588e0df696
Matthew <ng.manhtruong1996@gmail.com>
2024-02-22 20:36:26 +0700

    change svelte adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // import adapter from "@sveltejs/adapter-static";
-import adapter from "@sveltejs/adapter-cloudflare";
-// import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
